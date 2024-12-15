30f363674191a8ec644c6774b335e66f5f8ba36f
Bharti152k2 <nidhi15sak@gmail.com>
2024-12-15 19:46:58 +0530

    email client app initiated

---
+++ src/components/EmailList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/emailList.css";
+function EmailList() {
+  return (
+    <main>
+      <article className="filter-section">filter</article>
+      <section className="emails-list">
+        <article>mckdmkem</article>
+        <article>osdakdadjqlk</article>
+      </section>
+    </main>
+    // <main className="main-list-page">
+    //   <article className="filter-section">filter</article>
+    //   <section className="emails-list">
+    //     <article>mckdmkem</article>
+    //     <article>osdakdadjqlk</article>
+    //   </section>
+    // </main>
+  );
+}
+
+export default EmailList;
